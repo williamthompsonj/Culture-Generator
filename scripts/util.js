c480741ba77f6d413a79e2aed0967a3b15754f39
William Thompson <williamthompsonj@gmail.com>
2023-05-19 17:31:57 -0700

    Util Syntax
    - Update syntax in util.js to match syntax in the rest of the project

--- scripts/util.js
+++ scripts/util.js
@@ -1,161 +1,159 @@
 "use strict";
 // utility object namespace
-var util = {
-  /* Get element object using query syntax. Returns null if nothing found.
-    - "foo" gets the first element named <foo>
-    - "#foo" gets the first element with id="foo"
-    - ".foo" gets the first element with class="foo"
-    - [attr="foo"] gets the first element with attr="foo"
-    - tag[attr="foo"] gets the first element with <tag attr="foo">
-  */
-  getElem(id)
+var util = {};
+/* Get element object using query syntax. Returns null if nothing found.
+  - "foo" gets the first element named <foo>
+  - "#foo" gets the first element with id="foo"
+  - ".foo" gets the first element with class="foo"
+  - [attr="foo"] gets the first element with attr="foo"
+  - tag[attr="foo"] gets the first element with <tag attr="foo">
+*/
+util.getElem = function(id)
+{
+  return document.querySelector(id);
+};
+
+/* Get element value */
+util.getValue = function(id)
+{
+  let z = this.getElem(id);
+  if(z == null)
+    return '';
+
+  switch(z.tagName.toUpperCase())
   {
-    return document.querySelector(id);
-  },
+    // form element value
+    case 'TEXTAREA':
+    case 'INPUT':
+      return z.value;
+      break;
 
-  /* Get element value */
-  getValue(id)
+    case 'SELECT':
+      if(z.selectedIndex == -1)
+      {
+        // if nothing selected, default to first option
+        z.selectedIndex = 0;
+        z.value = z.options[0].value;
+      }
+      return z.value;
+      break;
+
+    // html tag contents
+    default:
+      return z.innerHTML;
+      break;
+  }
+};
+
+/* Get element inner HTML */
+util.getHtml = function(id)
+{
+  let z = this.getElem(id);
+  if(z == null)
+    return '';
+
+  return z.innerHTML;
+};
+
+/* Set element object */
+util.setElem = function(id, val)
+{
+  let z = this.getElem(id);
+  if(z == null)
+    return;
+  this.getElem(id) = val;
+};
+
+/* Set element value */
+util.setValue = function(id, val)
+{
+  let z = this.getElem(id);
+  if(z == null)
+    return;
+
+  switch(z.tagName.toUpperCase())
   {
-    let z = this.getElem(id);
-    if(z == null)
-      return '';
+    case 'TEXTAREA':
+    case 'INPUT':
+      z.value = val;
+      break;
 
-    switch(z.tagName.toUpperCase())
-    {
-      // form element value
-      case 'TEXTAREA':
-      case 'INPUT':
-        return z.value;
-        break;
-
-      case 'SELECT':
-        if(z.selectedIndex == -1)
+    case 'SELECT':
+      for(var i = 0; i != z.options.length; i++)
+      {
+        if (val == z.options[i].text)
         {
-          // if nothing selected, default to first option
-          z.selectedIndex = 0;
-          z.value = z.options[0].value;
+          z.selectedIndex = i;
+          z.options[i].selected = true;
         }
-        return z.value;
-        break;
-
-      // html tag contents
-      default:
-        return z.innerHTML;
-        break;
-    }
-  },
-
-  /* Get element inner HTML */
-  getHtml(id)
-  {
-    let z = this.getElem(id);
-    if(z == null)
-      return '';
-
-    return z.innerHTML;
-  },
-
-  /* Set element object */
-  setElem(id, val)
-  {
-    let z = this.getElem(id);
-    if(z == null)
-      return;
-    this.getElem(id) = val;
-  },
-
-  /* Set element value */
-  setValue(id, val)
-  {
-    let z = this.getElem(id);
-    if(z == null)
-      return;
-
-    switch(z.tagName.toUpperCase())
-    {
-      case 'TEXTAREA':
-      case 'INPUT':
-        z.value = val;
-        break;
-
-      case 'SELECT':
-        for(var i = 0; i != z.options.length; i++)
+        else
         {
-          if (val == z.options[i].text)
-          {
-            z.selectedIndex = i;
-            z.options[i].selected = true;
-          }
-          else
-          {
-            z.options[i].selected = false;
-          }
+          z.options[i].selected = false;
         }
-        break;
+      }
+      break;
 
-      default:
-        z.innerHTML = val;
-        break;
-    }
-  },
+    default:
+      z.innerHTML = val;
+      break;
+  }
+};
 
-  /* Set element inner HTML */
-  setHtml(id, val)
+/* Set element inner HTML */
+util.setHtml = function(id, val)
+{
+  let z = this.getElem(id);
+  if(z == null)
+    return;
+
+  z.innerHTML = val;
+};
+
+/* update value in objects when onChange occurs */
+util.updateElem = function(elem)
+{
+  var val = elem.value.trim();
+
+  if(val.toLowerCase() == 'true') // boolean true
+    val = true;
+  else if(val.toLowerCase() == 'false') // boolean false
+    val = false;
+  else if(isNaN(val)) // original string value
+    val = elem.value;
+  else
+    val = Number(val); // number
+
+  let arr = elem.id.split('_');
+  var index = elem.id.substring(arr[0].length+1);
+
+  switch(arr[0])
   {
-    let z = this.getElem(id);
-    if(z == null)
-      return;
+    case "gov":
+      government.props[index].value = val;
+      break;
 
-    z.innerHTML = val;
-  },
+    case "pop":
+      population.props[index].value = val;
+      break;
 
-  /* update value in objects when onChange occurs */
-  updateElem(elem)
+    case "rel":
+      religion.props[index].value = val;
+      break;
+  }
+};
+
+/* auto-populate form fields based on object props */
+util.fillForm = function(prefix, obj, precision = 2)
+{
+  let keys = Object.keys(obj.props);
+  for(var i = 0; i != keys.length; i++)
   {
-    var val = elem.value.trim();
+    let str = "#" + prefix + keys[i];
+    let val = obj.props[keys[i]].value;
 
-    if(val.toLowerCase() == 'true') // boolean true
-      val = true;
-    else if(val.toLowerCase() == 'false') // boolean false
-      val = false;
-    else if(isNaN(val)) // original string value
-      val = elem.value;
+    if(isNaN(val))
+      this.setValue(str, String(val).toTitleCase());
     else
-      val = Number(val); // number
-
-    let arr = elem.id.split('_');
-    var index = elem.id.substring(arr[0].length+1);
-
-    switch(arr[0])
-    {
-      case "gov":
-        government.props[index].value = val;
-        break;
-
-      case "pop":
-        population.props[index].value = val;
-        break;
-
-      case "rel":
-        religion.props[index].value = val;
-        break;
-    }
-  },
-
-  /* auto-populate form fields based on object props */
-  fillForm(prefix, obj, precision = 2)
-  {
-    let keys = Object.keys(obj.props);
-    for(var i = 0; i != keys.length; i++)
-    {
-      let str = "#" + prefix + keys[i];
-      let val = obj.props[keys[i]].value;
-
-      if(isNaN(val))
-        this.setValue(str, String(val).toTitleCase());
-      else
-        this.setValue(str, Number(val).toFixed(precision));
-    }
-  },
-
+      this.setValue(str, Number(val).toFixed(precision));
+  }
 };
